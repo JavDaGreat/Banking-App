21412bf9a9f1147748cd374a8ef4e201dc291ab7
Js-PC <1998javad@gmail.com>
2024-09-05 16:20:36 +0200

    comeplete bank card and implement links

--- components/BankCard.tsx
+++ components/BankCard.tsx
@@ -1,4 +1,5 @@
 import { formatAmount } from "@/lib/utils";
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
@@ -15,7 +16,36 @@ function BankCard({ account, userName, showBalance = true }: CreditCardProps) {
               {formatAmount(account.currentBalance)}
             </p>
           </div>
+
+          <article className="flex flex-col gap-2">
+            <div className="flex justify-between">
+              <h1 className="text-12 font-semibold text-white">{userName}</h1>
+              <h2 className="text-12 font-semibold text-white">●● / ●●</h2>
+            </div>
+            <p className="text-14 font-semibold tracking-[1.1px] text-white">
+              ●●●● ●●●● ●●●● <span className="text-16">1234</span>
+            </p>
+          </article>
         </div>
+        <div className="bank-card_icon">
+          <Image src="/icons/Paypass.svg" alt="Pay" width={20} height={24} />
+          <Image
+            src="/icons/mastercard.svg"
+            alt="Visa"
+            width={45}
+            height={32}
+            className="ml-5"
+          />
+        </div>
+        <Image
+          src="/icons/lines.png"
+          width={316}
+          height={190}
+          alt="lines"
+          className=" absolute top-0 left-0"
+        />
+
+        {/* COPY */}
       </Link>
     </div>
   );
