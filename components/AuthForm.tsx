d2466e31d5835d527797adb87313d9dd4a14d446
Javad <1998javad@gmail.com>
2024-10-29 14:26:48 +0100

    implement plaid and Dwolla functions

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -12,6 +12,7 @@ import { AuthFormSchema } from "@/lib/utils";
 import { Loader2 } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { signIn, signUp } from "@/lib/actions/user.actions";
+import PlaidLink from "./PlaidLink";
 
 function AuthForm({ type }: { type: "sign-in" | "sign-up" }) {
   const [user, setUser] = useState(null);
@@ -30,6 +31,18 @@ function AuthForm({ type }: { type: "sign-in" | "sign-up" }) {
   const onSubmit = async (data: z.infer<typeof formSchema>) => {
     setIsLoading(true);
     try {
+      const userData = {
+        firstName: data.firstName!,
+        lastName: data.lastName!,
+        address1: data.address1!,
+        city: data.city!,
+        state: data.state!,
+        postalCode: data.postalCode!,
+        dateOfBirth: data.dateOfBirth!,
+        ssn: data.ssn!,
+        email: data.email,
+        password: data.password!,
+      };
       if (type === "sign-in") {
         const response = await signIn({
           email: data.email,
@@ -38,7 +51,7 @@ function AuthForm({ type }: { type: "sign-in" | "sign-up" }) {
         if (response) router.push("/");
       }
       if (type === "sign-up") {
-        const newUser = await signUp(data);
+        const newUser = await signUp(userData);
         setUser(newUser);
       }
     } catch (error) {
@@ -73,7 +86,9 @@ function AuthForm({ type }: { type: "sign-in" | "sign-up" }) {
         </div>
       </header>
       {user ? (
-        <div className="flex flex-col gap-4">{/* PlaidLink */}</div>
+        <div className="flex flex-col gap-4">
+          <PlaidLink user={user} variant="primary" />
+        </div>
       ) : (
         <>
           <Form {...form}>
