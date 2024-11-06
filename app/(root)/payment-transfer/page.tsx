6637c796cd50ab8e793ab38daa18d1b2d9601d40
Javad <1998javad@gmail.com>
2024-11-06 12:21:20 +0100

    implement Transfer Funds page

--- app/(root)/payment-transfer/page.tsx
+++ app/(root)/payment-transfer/page.tsx
@@ -1,7 +1,31 @@
+import HeaderSection from "@/components/HeaderSection";
+import PaymentTransferForm from "@/components/PaymentTransferFrom";
+import { getAccounts } from "@/lib/actions/bank.actions";
+import { getLoggedInUser } from "@/lib/actions/user.actions";
 import React from "react";
 
-function Transfer() {
-  return <div>Transfer</div>;
-}
+const Transfer = async () => {
+  const loggedIn = await getLoggedInUser();
+  const accounts = await getAccounts({
+    userId: loggedIn.$id,
+  });
+
+  if (!accounts) return;
+
+  const accountsData = accounts?.data;
+
+  return (
+    <section className="payment-transfer">
+      <HeaderSection
+        title="Payment Transfer"
+        subtext="Please provide any specific details or notes related to the payment transfer"
+      />
+
+      <section className="size-full pt-5">
+        <PaymentTransferForm accounts={accountsData} />
+      </section>
+    </section>
+  );
+};
 
 export default Transfer;
